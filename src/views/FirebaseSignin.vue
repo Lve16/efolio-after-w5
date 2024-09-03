ea149ddd00a19f426f8f0261a5e0e1314131d23e
Lve16 <luv677103@gmail.com>
2024-09-03 21:38:01 +1000

    finish firebase authenticated

---
+++ src/views/FirebaseSignin.vue
@@ -0,0 +1,78 @@
+<!-- <template>
+    <h1>Sign in</h1>
+    <p><input type="text" placeholder="Email" v-model = "email"/></p>
+    <p><input type="password" placeholder="Password" v-model = "password"/></p>
+    <p><button @click="signin">Sign in via Firebase</button></p>
+</template>
+
+<script setup>
+import { ref } from "vue"
+import {getAuth, signInWithEmailAndPassword} from "firebase/auth"
+import { useRouter } from "vue-router";
+const email = ref("")
+const password = ref("")
+const router = useRouter()
+const auth = getAuth()
+const signin = () => {
+    signInWithEmailAndPassword(getAuth(), email.value, password.value)
+    .then((data) => {
+        console.log("Firebase Sign in Successful!")
+        router.push("/")
+        console.log(auth.currentUser)
+    }).catch((error) => {
+        console.log(error.code)
+    })
+};
+</script> -->
+
+<template>
+    <h1>Sign in</h1>
+    <p><input type="text" placeholder="Email" v-model="email"/></p>
+    <p><input type="password" placeholder="Password" v-model="password"/></p>
+    <p><button @click="signin">Sign in via Firebase</button></p>
+  </template>
+  
+  <script setup>
+  import { ref } from "vue";
+  import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+  import { getFirestore, doc, getDoc } from "firebase/firestore";  
+  import { useRouter } from "vue-router";
+  
+  const email = ref("");
+  const password = ref("");
+  const router = useRouter();
+  const auth = getAuth();
+  const db = getFirestore();  
+  
+  const signin = async () => {
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email.value, password.value);
+      alert("Login successful!")
+      console.log("Firebase Sign in Successful!");
+      console.log(auth.currentUser);
+  
+      const user = userCredential.user;
+  
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+  
+      if (docSnap.exists()) {
+        const userData = docSnap.data();
+        const userRole = userData.role;
+  
+        console.log("User Role:", userRole);
+
+        if (userRole === "admin") {
+          router.push("/admin"); 
+        } else {
+          router.push("/");  
+        }
+      } else {
+        console.log("No such document!");
+      }
+  
+    } catch (error) {
+      console.log(error.code);
+    }
+  };
+  </script>
