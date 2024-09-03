38b6b67a41f49efa84d4ccdb4e8e064eab18a5de
Lve16 <luv677103@gmail.com>
2024-09-03 21:39:38 +1000

    implement multi-role authenticated

--- src/components/BHeader.vue
+++ src/components/BHeader.vue
@@ -1,6 +1,4 @@
-<template>
-  <!-- Using Bootstrap's Header template (starter code) -->
-  <!-- https://getbootstrap.com/docs/5.0/examples/headers/ -->
+<!-- <template>
   <div class="container">
     <header class="d-flex justify-content-center py-3">
       <ul class="nav nav-pills">
@@ -13,13 +11,15 @@
             About
           </router-link>
         </li>
-        <li class="nav-item" v-if="!isLoggedIn">
-          <router-link to="/login" class="nav-link" active-class="active">
-            Login
+        <li class="nav-item">
+          <router-link to="/FireLogin" class="nav-link" active-class="active">
+            Firebase Login
           </router-link>
         </li>
-        <li class="nav-item" v-if="isLoggedIn">
-          <button @click="logout" class="nav-link btn btn-link">Logout</button>
+        <li class="nav-item">
+          <router-link to="/FireRegister" class="nav-link" active-class="active">
+            Firebase Register
+          </router-link>
         </li>
       </ul>
     </header>
@@ -64,24 +64,171 @@
 </style>
 
 <script>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+</script> -->
+
+<template>
+  <div class="container">
+    <header class="d-flex justify-content-center py-3">
+      <ul class="nav nav-pills">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link" active-class="active" aria-current="page">
+            Home (Week 5)
+          </router-link>
+        </li>
+        <li class="nav-item" v-if="loggedIn">
+          <router-link to="/about" class="nav-link" active-class="active">
+            About
+          </router-link>
+        </li>
+        <li class="nav-item" v-if="!loggedIn">
+          <router-link to="/FireLogin" class="nav-link" active-class="active">
+            Firebase Login
+          </router-link>
+        </li>
+        <li class="nav-item" v-if="!loggedIn">
+          <router-link to="/FireRegister" class="nav-link" active-class="active">
+            Firebase Register
+          </router-link>
+        </li>
+        <li class="nav-item" v-if="loggedIn && isAdmin">
+          <router-link to="/admin" class="nav-link" active-class="active">
+            Admin
+          </router-link>
+        </li>
+        <li class="nav-item" v-if="loggedIn">
+          <button @click="logout" class="nav-link btn btn-link">Logout</button>
+        </li>
+      </ul>
+    </header>
+  </div>
+</template>
+
+<script>
+// import { getAuth } from "firebase/auth";
+// import { getFirestore, doc, getDoc } from "firebase/firestore";
+
+// export default {
+//   data() {
+//     return {
+//       loggedIn: false,
+//       isAdmin: false
+//     };
+//   },
+//   async created() {
+//     const auth = getAuth();
+//     const db = getFirestore();
+    
+//     auth.onAuthStateChanged(async user => {
+//       if (user) {
+//         this.loggedIn = true;
+//         const docRef = doc(db, "users", user.uid);
+//         const docSnap = await getDoc(docRef);
+//         if (docSnap.exists()) {
+//           this.isAdmin = docSnap.data().role === "admin";
+//         } else {
+//           console.log("No such document!");
+//         }
+//       } else {
+//         this.loggedIn = false;
+//         this.isAdmin = false;
+//       }
+//     });
+//   },
+//   methods: {
+//     logout() {
+//       const auth = getAuth();
+//       auth.signOut().then(() => {
+//         this.$router.push('/');
+//       });
+//     }
+//   }
+// };
+import { getAuth } from "firebase/auth";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 export default {
-  setup() {
-    const router = useRouter();
-    const isLoggedIn = ref(!!localStorage.getItem('loggedInUser'));
-
-    const logout = () => {
-      localStorage.removeItem('loggedInUser'); 
-      router.push('/'); 
-      isLoggedIn.value = false;
-    };
-
+  data() {
     return {
-      isLoggedIn,
-      logout
+      loggedIn: false,
+      isAdmin: false,
+      currentUser: null, // store current user information
     };
+  },
+  async created() {
+    const auth = getAuth();
+    const db = getFirestore();
+    
+    auth.onAuthStateChanged(async user => {
+      if (user) {
+        this.loggedIn = true;
+        this.currentUser = user; 
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          this.isAdmin = docSnap.data().role === "admin";
+        } else {
+          console.log("No such document!");
+        }
+      } else {
+        this.loggedIn = false;
+        this.isAdmin = false;
+        this.currentUser = null; // clear current user information
+      }
+    });
+  },
+  methods: {
+    logout() {
+      if (this.currentUser) {
+        console.log("Current User before logout:", this.currentUser.email);
+      } else {
+        console.log("No user is currently logged in.");
+      }
+
+      const auth = getAuth();
+      auth.signOut().then(() => {
+        this.$router.push('/');
+        console.log("User has been logged out.");
+        this.currentUser = null; // clear current user information
+      });
+    }
   }
 };
+
 </script>
+
+<style scoped>
+.b-example-divider {
+  height: 3rem;
+  background-color: rgba(0, 0, 0, 0.1);
+  border: solid rgba(0, 0, 0, 0.15);
+  border-width: 1px 0;
+  box-shadow:
+    inset 0 0.5em 1.5em rgba(0, 0, 0, 0.1),
+    inset 0 0.125em 0.5em rgba(0, 0, 0, 0.15);
+}
+
+.form-control-dark {
+  color: #fff;
+  background-color: var(--bs-dark);
+  border-color: var(--bs-gray);
+}
+.form-control-dark:focus {
+  color: #fff;
+  background-color: var(--bs-dark);
+  border-color: #fff;
+  box-shadow: 0 0 0 0.25rem rgba(255, 255, 255, 0.25);
+}
+
+.bi {
+  vertical-align: -0.125em;
+  fill: currentColor;
+}
+
+.text-small {
+  font-size: 85%;
+}
+
+.dropdown-toggle {
+  outline: 0;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,101 @@
+// import { createRouter, createWebHistory } from 'vue-router'
+// import HomeView from '../views/HomeView.vue'
+// import AboutView from '../views/AboutView.vue'
+// import Login from '../views/Login.vue'
+// import FirebaseSignin from '../views/FirebaseSignin.vue'
+// import FirebaseRegiter from '../views/FirebaseRegiter.vue'
+// import Admin from '../views/Admin.vue'
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import { getFirestore, doc, getDoc } from "firebase/firestore";
+
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'Home',
+//     component: HomeView
+//   },
+//   {
+//     path: '/about',
+//     name: 'About',
+//     component: AboutView,
+//     meta: { requiresAuth: true } 
+//   },
+//   {
+//     path: '/login',
+//     name: 'Login',
+//     component: Login
+//   },
+//   {
+//     path: '/FireLogin',
+//     name: 'FireLogin',
+//     component: FirebaseSignin
+//   },
+//   {
+//     path: '/FireRegister',
+//     name: 'FireRegister',
+//     component: FirebaseRegiter
+//   },
+//   {
+//     path: '/admin',
+//     name: 'Admin',
+//     component: Admin,
+//     meta: { requiresAuth: true, requiresAdmin: true } 
+//   }
+// ]
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes
+// })
+
+// router.beforeEach((to, from, next) => {
+//   const auth = getAuth();
+//   const db = getFirestore();
+
+//   onAuthStateChanged(auth, async (user) => {
+//     if (to.matched.some(record => record.meta.requiresAuth)) {
+//       if (!user) {
+//         console.log("User not logged in, redirecting to login page.");
+//         next('/FireLogin');
+//       } else {
+//         if (to.matched.some(record => record.meta.requiresAdmin)) {
+//           const docRef = doc(db, "users", user.uid);
+//           const docSnap = await getDoc(docRef);
+
+//           if (docSnap.exists()) {
+//             const userRole = docSnap.data().role;
+//             console.log(`User role is ${userRole}`);
+//             if (userRole === 'admin') {
+//               next(); 
+//             } else {
+//               console.log("User is not admin, redirecting to home page.");
+//               next('/'); 
+//             }
+//           } else {
+//             console.log("No user role found, redirecting to home page.");
+//             next('/'); 
+//           }
+//         } else {
+//           next(); 
+//         }
+//       }
+//     } else {
+//       next(); 
+//     }
+//   });
+// });
+
+// export default router
+
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import Login from '../views/Login.vue'
+import FirebaseSignin from '../views/FirebaseSignin.vue'
+import FirebaseRegiter from '../views/FirebaseRegister.vue'
+import Admin from '../views/Admin.vue'
+import { getAuth } from "firebase/auth";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 const routes = [
   {
@@ -19,6 +113,22 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login
+  },
+  {
+    path: '/FireLogin',
+    name: 'FireLogin',
+    component: FirebaseSignin
+  },
+  {
+    path: '/FireRegister',
+    name: 'FireRegister',
+    component: FirebaseRegiter
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin,
+    meta: { requiresAuth: true, requiresAdmin: true } 
   }
 ]
 
@@ -27,13 +137,46 @@ const router = createRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
-  const loggedIn = localStorage.getItem('loggedInUser'); 
-  if (to.matched.some(record => record.meta.requiresAuth) && !loggedIn) {
-    next('/login');
+router.beforeEach(async (to, from, next) => {
+  const auth = getAuth();
+  const db = getFirestore();
+  const user = auth.currentUser;
+
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (!user) {
+      console.log("User status: not logged in -> redirecting to login page.");
+      alert("Please login first!")
+      return next('/FireLogin');
+    } else {
+      if (to.matched.some(record => record.meta.requiresAdmin)) {
+        try {
+          const docRef = doc(db, "users", user.uid);
+          const docSnap = await getDoc(docRef);
+
+          if (docSnap.exists()) {
+            const userRole = docSnap.data().role;
+            console.log(`User role is ${userRole}`);
+            if (userRole === 'admin') {
+              return next(); 
+            } else {
+              console.log("User role is not admin, redirecting to home page.");
+              return next('/'); 
+            }
+          } else {
+            console.log("No user role found, redirecting to home page.");
+            return next('/'); 
+          }
+        } catch (error) {
+          console.error("Error fetching user role: ", error);
+          return next('/'); 
+        }
+      } else {
+        return next(); 
+      }
+    }
   } else {
-    next();
+    return next(); 
   }
 });
 
-export default router
+export default router;
