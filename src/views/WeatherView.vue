4efadbfe871cc224f7f9a338db5c0a077f9c4433
Lve16 <luv677103@gmail.com>
2024-10-16 00:09:35 +1100

    fix some problems on weatherView.vue

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -72,7 +72,7 @@ export default {
     //Get the current weather icon using the API link
     iconUrl() {
       return this.weatherData
-        ? `http://api.openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
+        ? `https://api.openweathermap.org/img/w/${this.weatherData.weather[0].icon}.png`
         : null;
     },
   },
@@ -92,7 +92,7 @@ export default {
         navigator.geolocation.getCurrentPosition(async (position) => {
           const { latitude, longitude } = position.coords;
           //API link to obtain the current weather based on the current location browser identified 
-          const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
+          const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apikey}`;
           //await means wait for the fetchWeatherData method to complete before proceeding
           await this.fetchWeatherData(url);
         });
@@ -101,7 +101,7 @@ export default {
 
     async searchByCity() {
       if (this.city) {
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(this.city)}&appid=${apikey}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(this.city)}&appid=${apikey}`;
         await this.fetchWeatherData(url);
       } else {
         alert("Please enter a valid city name!");
