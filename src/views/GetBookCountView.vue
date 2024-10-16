a101b23fe7781a3a977746d586d500966acc7acd
Lve16 <luv677103@gmail.com>
2024-10-16 23:49:26 +1100

    Trigger deployment

--- src/views/GetBookCountView.vue
+++ src/views/GetBookCountView.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
       <h1>Book Counter</h1>
-      <button @click="getBookCount">Get Book Count</button>
+      <button @click="getBookCount">Get Books Count</button>
       <p v-if="count !== null">Total number of books: {{ count }}</p>
       <p v-if="error">{{ error }}</p>
     </div>
